e2c4e3bfb6af4a50a7f63735893fbdaa5319bcbf
Igor Fernandes <joaobatista22072003@gmail.com>
2026-04-12 21:51:08 -0300

    feat: corrigindo seeder de bairros

--- database/seeders/BairrosSeeder.php
+++ database/seeders/BairrosSeeder.php
@@ -13,41 +13,11 @@ class BairrosSeeder extends Seeder
         $bairros = [
 
             ['nome' => 'Centro', 'taxa' => 2.00],
-            ['nome' => 'Todos os Santos', 'taxa' => 3.00],
-            ['nome' => 'Funcionários', 'taxa' => 3.00],
-
-            ['nome' => 'Major Prates', 'taxa' => 4.00],
-            ['nome' => 'São José', 'taxa' => 4.00],
-            ['nome' => 'Santa Rita', 'taxa' => 4.00],
-            ['nome' => 'Delfino Magalhães', 'taxa' => 4.00],
-            ['nome' => 'Canelas', 'taxa' => 4.00],
-
-            ['nome' => 'Independência', 'taxa' => 5.00],
-            ['nome' => 'Maracanã', 'taxa' => 5.00],
-            ['nome' => 'Renascença', 'taxa' => 5.00],
-            ['nome' => 'Jardim Palmeiras', 'taxa' => 5.00],
-            ['nome' => 'Jardim Primavera', 'taxa' => 5.00],
-
-            ['nome' => 'Augusta Mota', 'taxa' => 6.00],
-            ['nome' => 'Santos Reis', 'taxa' => 6.00],
-            ['nome' => 'Vila Oliveira', 'taxa' => 6.00],
-            ['nome' => 'Planalto', 'taxa' => 6.00],
-            ['nome' => 'Esplanada', 'taxa' => 6.00],
-
-            ['nome' => 'Ibituruna', 'taxa' => 7.00],
-            ['nome' => 'Monte Carmelo', 'taxa' => 7.00],
-            ['nome' => 'Cidade Industrial', 'taxa' => 7.00],
-            ['nome' => 'Jaraguá', 'taxa' => 7.00],
-            ['nome' => 'Vila Atlântida', 'taxa' => 7.00],
-
-            ['nome' => 'Nossa Senhora Aparecida', 'taxa' => 8.00],
-            ['nome' => 'São Geraldo', 'taxa' => 8.00],
-            ['nome' => 'São Judas', 'taxa' => 8.00],
-            ['nome' => 'Santa Lúcia', 'taxa' => 8.00],
-            ['nome' => 'Roxo Verde', 'taxa' => 8.00],
-
-            ['nome' => 'Vila Regina', 'taxa' => 9.00],
-            ['nome' => 'Morada do Parque', 'taxa' => 9.00],
+            ['nome' => 'Fazendinha', 'taxa' => 3.00],
+            ['nome' => 'São Geraldo', 'taxa' => 3.00],
+            ['nome' => 'Bom Jesus', 'taxa' => 4.00],
+            ['nome' => 'Florestal', 'taxa' => 4.00],
+            ['nome' => 'Colinas', 'taxa' => 5.00],
         ];
 
         foreach ($bairros as $bairro) {
